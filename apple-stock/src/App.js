018859b6f04a9606cf6986d2e4abceeb8ee1fc6e
Yevgeniy <yev.bor@yandex.ru>
2023-03-14 14:09:17 +0200

    create tab navigation

--- apple-stock/src/App.js
+++ apple-stock/src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import TabsNavigation from './components/TabsNavigation';
 
 
 function App() {
+  // const [history, setHistory] = useState(false);
+  // const [overview, setOverview] = useState(true);
+
   return (
     <div className="App">
       <Header/>
+      <TabsNavigation>
+        {/* {history && <HistoryTab/>}
+        {overview && <OverviewTab/>} */}
+      </TabsNavigation>
     </div>
   );
 }
